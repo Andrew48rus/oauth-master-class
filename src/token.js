32a15b68ba130971f93deba5e982ec9828b06dba
Andrew48rus <52998648+Andrew48rus@users.noreply.github.com>
2023-03-05 14:09:48 +0300

    Update token.js

--- src/token.js
+++ src/token.js
@@ -1,3 +1,19 @@
-window.onload = () => {
-  YaSendSuggestToken("https://oauth-master-class-ox7019rzz-andrew48rus.vercel.app/");
-}; 
+YaAuthSuggest.init({
+         client_id: 'c46f0c53093440c39f12eff95a9f2f93',
+         response_type: 'token',
+         redirect_uri: 'https://oauth-master-class-qn0cjn08m-andrew48rus.vercel.app/token.html'
+      },
+      'https://oauth-master-class-qn0cjn08m-andrew48rus.vercel.app/', {
+         view: 'button',
+         parentId: 'container',
+         buttonView: 'main',
+         buttonTheme: 'light',
+         buttonSize: 'm',
+         buttonBorderRadius: 0
+         }
+   )
+   .then(({
+      handler
+   }) => handler())
+   .then(data => console.log('Сообщение с токеном', data))
+   .catch(error => console.log('Обработка ошибки', error));
