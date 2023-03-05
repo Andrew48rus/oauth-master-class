ba6f93931aa3839ad24661337d1657f38b803ef9
Andrew48rus <52998648+Andrew48rus@users.noreply.github.com>
2023-03-05 16:40:48 +0300

    Update token.js

--- src/token.js
+++ src/token.js
@@ -1,3 +1,8 @@
 window.onload = () => {
   YaSendSuggestToken("https://oauth-master-class-git-main-andrew48rus.vercel.app")
+    .then(({
+      handler
+   }) => handler())
+   .then(data => console.log('Сообщение с токеном', data))
+   .catch(error => console.log('Обработка ошибки', error));
 };
